f1063c678830ab858eedc58375a942b615402c66
hzue <zue.hterry@gmail.com>
2019-03-15 03:50:54 +0800

    feat: add bash

--- install.sh
+++ install.sh
@@ -12,11 +12,17 @@ fi
 rm -rf ${HOME}/.vim
 rm -rf ${HOME}/.vimrc
 rm -rf ${HOME}/.config/nvim/init.vim
+rm -rf ${HOME}/.bashrc
+rm -rf ${HOME}/.alias
+rm -rf ${HOME}/.gitconfig
 
 ## Link/Copy settings
 mkdir -p ${HOME}/.config/nvim
 
 ln -s ${TOP_DIR}/vim/vimrc      ${HOME}/.vimrc
 ln -s ${TOP_DIR}/vim/init.vim   ${HOME}/.config/nvim/init.vim
+ln -s ${TOP_DIR}/bash/.bashrc   ${HOME}/.bashrc
+ln -s ${TOP_DIR}/bash/.alias    ${HOME}/.alias
+ln -s ${TOP_DIR}/git/gitconfig  ${HOME}/.gitconfig
 
 ${VIM_BIN} +PlugInstall +qall
